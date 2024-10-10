d6931d972d12ad6498cefe8dc343c3134a441d46
Benaka prasad M <155962113+Benakaprasad@users.noreply.github.com>
2024-10-10 22:41:29 +0530

    Update linkedlistoperation.c

--- linkedlistoperation.c
+++ linkedlistoperation.c
@@ -7,3 +7,53 @@ struct node* link;
 };
 struct node* create(int data)
 {
+struct node* head=NULL;
+struct node* node=malloc(sizeof(staruct* node));
+printf("enter the data to the node");
+scanf("%d",&node->data);
+node->link=NULL;;
+return node;
+}
+void insert(struct node** head,int data)
+{
+struct node* newnode=malloc(sizeof(struct* node));
+if(*head==NULL)
+{
+*head=newnode;
+}
+  else
+{
+for(int i=0;i<data;i++)
+{
+printf("enter the element to be inserted:");
+scanf("%d",temp->data);
+temp=temp->link;
+temp=*head;
+}
+  printf("\n");
+}
+}
+void display(struct node* head)
+{
+struct node* temp=head;
+printf("the elements are:\n");
+printf("%d",temp->data);
+temp=temp->link;
+}
+int main()
+{
+struct node* head;
+head=create();
+insert(&head);
+display();
+return 0;
+}
+
+
+  
+
+
+
+
+
+
